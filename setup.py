da8e246f6d3269d539923e9df5cf625e20387dfb
Amit Singh <amitsngh1799@gmail.com>
2025-06-04 01:34:53 +0530

    Error Logging and Exception Handling Added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amit Singh",
+    author_email="amitjordanthegr8@gmail.com",
+    packages=find_packages()
+)
+
+
+# mongodb+srv://amitjordanthegr8:<db_password>@cluster0.pwdnt7n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
